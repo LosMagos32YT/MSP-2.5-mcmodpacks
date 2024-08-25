39f97f5fbc61b7e0a1b894ac3effa6de134c93c6
ksmagos32 <66799146+LosMagos32YT@users.noreply.github.com>
2024-08-25 23:07:06 +0000

    Mundo guardado 2024-08-25

--- server.py
+++ server.py
@@ -9,7 +9,7 @@
 # Código          Lugar
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
-# au		      Australia (Sydney)
+# au		      Australia (Sydney)stop
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
